cbdabda7729ef2fafe5349904ed6e20266ae54d3
w-wander-r <wanderexe68@gmail.com>
2025-01-17 16:13:04 +0200

    created clear app(backend first)

---
+++ SpringReactTodoApp/src/main/java/SpringReactTodoApp/SpringReactTodoAppApplication.java
@@ -0,0 +1,13 @@
+package SpringReactTodoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactTodoAppApplication.class, args);
+	}
+
+}
